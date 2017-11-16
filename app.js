7b5ce496c8f28b9fa78bbd1eae991cf051d7f6fd
Vuk Stanković <vuks89@gmail.com>
2017-11-16 22:55:25 +0100

    Added Orbitron and EJS template engine

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -19,7 +19,7 @@ app.use(function(req, res, next) {
   next();
 });
 
-// uncomment after placing your favicon in /public
+// TODO uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
