a5794b603b34a08ab00635ffd6b42ffaf4db9d68
Vuk Stanković <vuks89@gmail.com>
2017-12-03 00:12:54 +0100

    Only add client to client list after it's been logged in

--- controllers/socket.js
+++ controllers/socket.js
@@ -1,4 +1,5 @@
 'use strict';
+const playfab = require('playfab-sdk/Scripts/PlayFab/PlayFabClient');
 module.exports = {
   sockets: {},
   io: null,
@@ -7,18 +8,30 @@ module.exports = {
     console.log('Socket server running');
 
     this.io.on('connection', (socket) => {
-      this.sockets[socket.id] = {
-        id: socket.id
-      };
       socket.emit('connected', { status: true });
       socket.on('identify', (data) => {
         if (data.id) {
-          console.log(data);
-          socket.playfabId = data.id;
-          this.sockets[socket.id] = {
-            id: socket.id,
-            userId: data.id
-          };
+          playfab.GetPlayerProfile({
+            PlayFabId : data.id,
+            ProfileConstraints : {
+              ShowDisplayName : true,
+              ShowLinkedAccounts: true
+            }
+          }, (error, response) => {
+            if(error) { console.log(error); }
+            else {
+              socket.playfabId = data.id;
+              delete response.data.PlayerProfile.TitleId;
+              if (response.data.PlayerProfile.LinkedAccounts && response.data.PlayerProfile.LinkedAccounts[0].Username) {
+                response.data.PlayerProfile.DisplayName = response.data.PlayerProfile.LinkedAccounts[0].Username;
+              }
+              this.sockets[socket.id] = {
+                id: socket.id,
+                userId: data.id,
+                profile: response.data.PlayerProfile
+              };
+            }
+          });
         }
       });
       socket.on('disconnect', () => {
