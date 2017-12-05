a911fe5ff60be171f35cf1b64a8de3f09f51df06
Vuk Stanković <vuks89@gmail.com>
2017-12-05 20:38:17 +0100

    Emit move events

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -14,7 +14,7 @@ var socket;
 
 })();
 
-socket = io.connect('http://localhost:3000/');
+socket = io.connect();
 socket.on('connected', function (data) {
   if (data.status) {
     socket.emit('identify', { id: userId });
