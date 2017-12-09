3c5a58e9f64df79e5ef03845b690fd035c6096f4
Vuk Stanković <vuks89@gmail.com>
2017-12-09 09:22:54 +0100

    Undo double

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -749,15 +749,17 @@ mainState.prototype = {
     this.undoDouble();
   },
   undoDouble: function () {
-    if (this.doubleActive[side]) {
+    if (this.doubleActive[0]) {
       this.paddleLeftSprite.key = graphicAssets.paddleName;
       this.paddleLeftSprite.loadTexture(graphicAssets.paddleName, 0);
       this.paddleLeftSprite.height = this.originalPaddleHeight;
+      this.doubleActive[0] = false;
     }
-    if (this.doubleActive[side]) {
+    if (this.doubleActive[1]) {
       this.paddleRightSprite.key = graphicAssets.paddleRightName;
       this.paddleRightSprite.loadTexture(graphicAssets.paddleRightName, 0);
       this.paddleRightSprite.height = this.originalPaddleHeight;
+      this.doubleActive[1] = false;
     }
   },
   processHor: function (side) {},
