3a3f8b79d719cf4036c12cc1f78c9a83d88ffb28
Vuk Stanković <vuks89@gmail.com>
2017-12-09 23:22:08 +0100

    Fix faulty game start

--- controllers/socket.js
+++ controllers/socket.js
@@ -93,6 +93,7 @@ module.exports = {
       });
       socket.on('accept', (data) => {
         // Make sure room has exactly two members before starting a game
+        console.log(this.io.sockets.adapter.rooms[data.host].sockets);
         if (Object.keys(this.io.sockets.adapter.rooms[data.host].sockets).length >= 2) {
           setTimeout(() => {
             console.log('start_game');
