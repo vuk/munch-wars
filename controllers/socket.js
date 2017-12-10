379c78f8c4a3001990f3d974f476279a8151f1b4
Vuk Stanković <vuks89@gmail.com>
2017-12-10 15:47:12 +0100

    Refactoring of state synchronization

--- controllers/socket.js
+++ controllers/socket.js
@@ -129,7 +129,7 @@ module.exports = {
             ball: {}
           };
         }
-        if(this.state[data.id]['paddle'][data.side] && this.state[data.id]['paddle'][data.side].time < data.time) {
+        if(this.state[data.id]['paddle'][data.side] && (!this.state[data.id]['paddle'][data.side].time || this.state[data.id]['paddle'][data.side].time < data.time)) {
           this.state[data.id]['paddle'][data.side] = data;
         }
       });
@@ -140,7 +140,7 @@ module.exports = {
             ball: {}
           };
         }
-        if (this.state[data.id]['ball'].time < data.time) {
+        if (!this.state[data.id]['ball'].time || this.state[data.id]['ball'].time < data.time) {
           this.state[data.id]['ball'] = data;
         }
         //this.io.to(data.id).emit('ball', data);
