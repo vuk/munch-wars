1881cd65efd60225d4994ad74271b2f060b7a3fe
Vuk Stanković <vuks89@gmail.com>
2017-12-10 12:38:34 +0100

    Update only velocity of ball

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -381,13 +381,9 @@ mainState.prototype = {
         self.ballSprite.body.velocity.y = 0;
         socket.on('ball', function (data) {
           self.ballSprite.visible = data.visible;
-          /*if (Math.abs(self.ballSprite.body.x - data.x) < 50 && Math.abs(self.ballSprite.body.y - data.y) < 50)*/
-          //game.physics.arcade.moveToXY(self.ballSprite, data.x, data.y, 0, 40);
           /*self.ballSprite.x = data.x;
           self.ballSprite.y = data.y;*/
           self.ballSprite.body.velocity.set(data.velocityX, data.velocityY);
-          /*game.ballSprite.body.velocity.x = data.velocityX;
-          game.ballSprite.body.velocity.y = data.velocityY;*/
         });
         isBallListenerSet = true;
       }
