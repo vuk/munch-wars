340a3a32c911664a129c03de74662a71d33b7e52
Vuk Stanković <vuks89@gmail.com>
2017-12-10 14:53:07 +0100

    Refactoring of state synchronization

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -265,6 +265,7 @@ mainState.prototype = {
         self.ballSprite.x = data.ball.x;
         self.ballSprite.y = data.ball.y;
         self.ballSprite.body.velocity.set(data.ball.velocityX, data.ball.velocityY);
+        console.log(self.ballSprite);
       }
     });
     socket.on('score', function (data) {
