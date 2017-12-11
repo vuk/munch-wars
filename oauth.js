91a7659fde3e94919659cfebe2f612f0b2a9cc84
Vuk Stanković <vuks89@gmail.com>
2017-12-11 23:25:16 +0100

    Speed of computer on 65% of players

--- oauth.js
+++ oauth.js
@@ -2,7 +2,7 @@ const ids = {
   facebook: {
     clientID: '140871050019105',
     clientSecret: '3bb7bb9171e0bce345caf862b1bc8a60',
-    callbackURL: 'http://207.154.237.15/auth/social-login'
+    callbackURL: 'http://munchwars.rs/auth/social-login'
   }
 };
 
