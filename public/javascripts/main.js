ce4bd8f2c8f058977e8b32c7ed37cf53b6529711
Vuk Stanković <vuks89@gmail.com>
2017-12-11 22:15:21 +0100

    Fixed issues 40 & 41 regarding weekly and daily points on profile page

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -119,6 +119,32 @@ if (jQuery('#games').length > 0 && !noevent) {
   }
 }
 
+function getRankIcon (tp) {
+  var rank;
+  if(tp <= 100) {
+    rank = '/assets/ranks/1.png';
+  }
+  if (tp >= 101 && tp <= 200) {
+    rank = '/assets/ranks/2.png';
+  }
+  if (tp >= 201 && tp <= 300) {
+    rank = '/assets/ranks/3.png';
+  }
+  if (tp >= 301 && tp <= 400) {
+    rank = '/assets/ranks/4.png';
+  }
+  if (tp >= 401 && tp <= 500) {
+    rank = '/assets/ranks/5.png';
+  }
+  if (tp >= 501 && tp <= 600) {
+    rank = '/assets/ranks/6.png';
+  }
+  if (tp > 600) {
+    rank = '/assets/ranks/7.png';
+  }
+  return rank;
+}
+
 // Load opponents
 if (jQuery('#opponents').length > 0) {
   $.get('/profile/actives', function (data) {
@@ -265,8 +291,8 @@ if($('.fetch-rank-row').length > 0) {
           data.daily.Position = '0' + data.daily.Position;
         }
         $(self).children().find('.daily-rank').html(data.daily.Position);
-        while(data.daily.StatValue.toString().length < 5) {
-          data.daily.StatValue = '0' + data.daily.StatValue;
+        while(data.daily.Position.toString().length < 5) {
+          data.daily.Position = '0' + data.daily.StatValue;
         }
         $(self).children().find('.daily-points').html(data.daily.StatValue);
       } else {
